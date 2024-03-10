904a171c2d2b1bd6588fc1a9193c9d373bc04369
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2024-03-09 21:09:23 -0700

    socket io tests

---
+++ pages/api/socketio.js
@@ -0,0 +1,24 @@
+import { Server } from "socket.io";
+
+export let io;
+
+const ioHandler = (req, res) => {
+	// check if server already started
+	if (!res.socket.server.io) {
+		console.log("Starting socket.io server");
+
+		// starts server
+		io = new Server(res.socket.server);
+		res.socket.server.io = io;
+
+		io.on("connection", socket => {
+			socket.on("echo", message => {
+				console.log("echo", message);
+				socket.emit("echo", message);
+			});
+		});
+	}
+	res.end();
+};
+
+export default ioHandler;
