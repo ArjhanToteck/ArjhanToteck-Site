846e154e70239549008e4ed11150021ea684e2a7
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2024-09-03 23:40:40 -0600

    work on code block, cumulus

--- components/CodeBlock.js
+++ components/CodeBlock.js
@@ -4,9 +4,11 @@ import React, { useRef } from "react";
 import hljs from "highlight.js/lib/common";
 
 export default function CodeBlock({ code }) {
+	// highlight code
 	const highlightedCode = hljs.highlightAuto(code).value;
 
 	const codeRef = useRef(null);
+	const copyIconRef = useRef(null);
 
 	function copy(text) {
 		navigator.clipboard.writeText(text)
@@ -17,13 +19,21 @@ export default function CodeBlock({ code }) {
 
 	function handleCopy() {
 		if (codeRef.current) {
+			// copy code
 			copy(codeRef.current.innerText);
+		}
+
+		if (copyIconRef.current) {
+			// change copy icon to check mark
+			copyIconRef.current.src = "/images/checkMark.svg";
 		}
 	}
 
 	return (
-		<div className="round" style={{ backgroundColor: "#131313", color: "white", padding: "25px", position: "relative" }}>
-			<img src="/images/copy.svg" style={{ position: "absolute", top: "10px", right: "10px" }} onClick={handleCopy} title="Copy"></img>
+		<div className="round red" style={{ backgroundColor: "#131313", color: "white", padding: "30px", position: "relative" }}>
+			<button style={{ position: "absolute", top: "10px", right: "10px" }} onClick={handleCopy}>
+				<img ref={copyIconRef} src="/images/copy.svg" title="Copy"></img>
+			</button>
 			<pre>
 				<code ref={codeRef} style={{ whiteSpace: "pre-wrap" }} dangerouslySetInnerHTML={{
 					__html: highlightedCode
