2445d850e7aac9706fefbbbfcc9bc9176f68a2f6
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2024-09-06 22:36:11 -0600

    move to src folder, set up copy system to use instead of symlinks

---
+++ prebuild/configureProjects.js
@@ -0,0 +1,52 @@
+import { existsSync, readdir, statSync } from "fs";
+import cpx from "cpx2";
+
+const projectFolder = "./projects/";
+
+// get items in project folder
+export default function configureProjects() {
+	readdir(projectFolder, (err, items) => {
+		if (err) {
+			console.error("Error reading projects folder: ", err);
+			return;
+		}
+
+		// loop through items
+		items.forEach(item => {
+			const path = projectFolder + item;
+
+			// get item info
+			let stats = statSync(path);
+
+			// check if directory
+			if (stats.isDirectory()) {
+				console.log("Configuring project " + item);
+
+				// app folder
+				if (existsSync(path + "/app")) {
+					copyPath(path + "/app/**", "src/app/projects/" + item);
+				}
+
+				// api folder
+				if (existsSync(path + "/api")) {
+
+					copyPath(path + "/api/**", "src/pages/api/projects/" + item);
+				}
+
+				// public folder
+				if (existsSync(path + "/public")) {
+
+					copyPath(path + "/public/**", "public/projects/" + item);
+				}
+			}
+		});
+	});
+}
+
+function copyPath(source, destination) {
+	if (process.env.NODE_ENV == "development") {
+		cpx.watch(source, destination);
+	} else {
+		cpx.copySync(source, destination);
+	}
+}
---
+++ prebuild/prebuild.js
@@ -0,0 +1,4 @@
+import "dotenv/config";
+import configureProjects from "./configureProjects.js";
+
+configureProjects();
