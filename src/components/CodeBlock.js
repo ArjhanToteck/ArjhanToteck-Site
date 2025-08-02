9ae9c4115d6107467e1c89c9bccb1b48fb0cdeff
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2025-08-01 23:13:19 -0600

    Update CodeBlock.js
    added download button, button cooldowns, scrollbar

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -1,45 +1,115 @@
 "use client";
 
 import Image from "next/image";
-import React, { useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import hljs from "highlight.js/lib/common";
 
-export default function CodeBlock({ code }) {
-	// highlight code
-	const highlightedCode = hljs.highlightAuto(code).value;
+// TODO: set language
+export default function CodeBlock({ code, showCopyButton = true, showDownloadButton = false, fileName = "code.txt", maxCodeHeight = "none", language }) {
+	const [highlightedCode, setHighlightedCode] = useState("");
 
 	const codeRef = useRef(null);
 	const copyIconRef = useRef(null);
+	const downloadIconRef = useRef(null);
 
-	function copy(text) {
-		navigator.clipboard.writeText(text)
-			.catch((error) => {
-				console.error("Failed to copy text: ", error);
-			});
-	}
-
-	function handleCopy() {
-		if (codeRef.current) {
-			// copy code
-			copy(codeRef.current.innerText);
+	// highlight code
+	useEffect(() => {
+		if (language) {
+			// set language
+			setHighlightedCode(hljs.highlight(code, { language }).value);
+		} else {
+			// auto detect language
+			setHighlightedCode(hljs.highlightAuto(code).value);
 		}
-
-		if (copyIconRef.current) {
-			// change copy icon to check mark
-			copyIconRef.current.src = "/images/checkMark.svg";
-		}
-	}
+	}, [code]);
 
 	return (
 		<div className="round red" style={{ textAlign: "left", backgroundColor: "#131313", color: "white", padding: "30px", position: "relative" }}>
-			<button style={{ position: "absolute", top: "10px", right: "10px" }} onClick={handleCopy}>
-				<Image ref={copyIconRef} src="/images/copy.svg" title="Copy" alt="Copy" width="25" height="25" />
-			</button>
-			<pre>
-				<code ref={codeRef} style={{ whiteSpace: "pre-wrap" }} dangerouslySetInnerHTML={{
+
+			<div style={{
+				position: "absolute",
+				top: "10px",
+				right: "10px",
+				display: "flex",
+				flexDirection: "row-reverse",
+				gap: "5px"
+			}}>
+				<button style={{ display: showCopyButton ? "block" : "none" }} onClick={handleCopy}>
+					<Image ref={copyIconRef} src="/images/copy.svg" title="Copy" alt="Copy" width="25" height="25" />
+				</button>
+
+				<button style={{ display: showDownloadButton ? "block" : "none" }} onClick={handleDownload}>
+					<Image ref={downloadIconRef} src="/images/download.svg" title="Copy" alt="Copy" width="25" height="25" />
+				</button>
+			</div>
+
+
+			<pre style={{ overflowY: "auto", maxHeight: maxCodeHeight }}>
+				<code className="language-gcode" ref={codeRef} style={{ whiteSpace: "pre-wrap" }} dangerouslySetInnerHTML={{
 					__html: highlightedCode
 				}} />
 			</pre>
 		</div>
 	);
+
+
+	function handleCopy() {
+		if (codeRef.current) {
+			// copy code
+			navigator.clipboard.writeText(codeRef.current.innerText)
+				.catch((error) => {
+					console.error("Failed to copy text: ", error);
+				});
+		}
+
+		buttonCheckMark(copyIconRef, "/images/copy.svg");
+	}
+
+
+	function handleDownload() {
+		if (codeRef.current) {
+			// download code
+
+			// create blob url for code
+			const blob = new Blob([codeRef.current.innerText], { type: "text/plain" });
+			const url = URL.createObjectURL(blob);
+
+			// create link with file name
+			const a = document.createElement("a");
+			a.href = url;
+			a.download = fileName;
+
+			// simulate click to download
+			document.body.appendChild(a);
+			a.click();
+			document.body.removeChild(a);
+
+			// clean up url object
+			URL.revokeObjectURL(url);
+		}
+
+		buttonCheckMark(downloadIconRef, "/images/download.svg");
+	}
+
+	function buttonCheckMark(iconRef, originalSrc, cooldown = 1000) {
+		if (iconRef.current) {
+
+			// change button icon to check mark
+			iconRef.current.src = "/images/checkMark.svg";
+
+			// reset old cooldown
+			if (iconRef.current.cooldown) {
+				clearTimeout(iconRef.current.cooldown);
+			}
+
+			// wait for cooldown
+			iconRef.current.cooldown = setTimeout(() => {
+				console.log("asd");
+				// revert src
+				if (iconRef.current) {
+					iconRef.current.src = originalSrc;
+				}
+			}, cooldown);
+		}
+	}
 }
