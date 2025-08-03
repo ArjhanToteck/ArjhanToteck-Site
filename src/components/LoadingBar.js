ea770407b06ef517ab54883c730f4a4690b8f8f5
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2025-08-02 19:00:03 -0600

    update loading bar

--- src/components/LoadingBar.js
+++ src/components/LoadingBar.js
@@ -2,7 +2,7 @@ export default function LoadingBar({ percentage, label = "Loading...", hidden = 
 	return (
 		<div style={{ width: "40%", display: hidden ? "none" : "block" }}>
 			<div className="round" style={{ backgroundColor: "#131313", color: "white", width: "100%", height: "10px" }}>
-				<div className="red round" style={{ color: "white", width: percentage + "%", height: "10px", position: "relative", top: "-10px", left: "-10px" }} />
+				<div className="red round" style={{ color: "white", width: (percentage * 100) + "%", height: "10px", position: "relative", top: "-10px", left: "-10px" }} />
 			</div>
 			<p>{label}</p>
 		</div>
